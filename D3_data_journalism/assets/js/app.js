24d102e179678621accdd46d504d79941877a5be
maylacdao <may.lacdao@gmail.com>
2021-01-28 17:26:04 -0500

    Debug line 24

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -21,7 +21,7 @@ var chart = d3.select("#scatter")
 
 var svg = chart.append("svg")
                 .attr("width", svgWidth)
-                .att("height", svgHeight);
+                .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
                     .attr("transform", `translate(${margin.left}, ${margin.top})`);
