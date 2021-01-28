e2d52d194f24fb67912dfbb5ba50e39be309f7d0
maylacdao <may.lacdao@gmail.com>
2021-01-28 16:26:17 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -15,4 +15,14 @@ var width = svgWidth - margin.right - margin.left;
 var height = svgHeight - margin.top - margin.bottom;
 
 
-let chart = d3.select
+let chart = d3.select("#scatter")
+                .append("div")
+                .classed("chart", true);
+
+let svg = chart.append("svg")
+                .attr("width", svgWidth)
+                .att("height", svgHeight);
+
+let chartGroup = svg.append("g")
+                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
