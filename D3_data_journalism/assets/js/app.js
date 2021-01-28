70c10593130e6ed6227f6d53e2360660efe214fa
maylacdao <may.lacdao@gmail.com>
2021-01-28 16:23:13 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,18 @@
-// @TODO: YOUR CODE HERE!
+//Specify initial svg size
+var svgWidth = 960;
+var svgHeight = 650;
+
+//Set margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+
+var width = svgWidth - margin.right - margin.left;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+let chart = d3.select
